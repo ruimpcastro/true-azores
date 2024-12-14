dd8ddc0c930f57d0cc0ed71bd5e972a8c4174c15
Rui Castro <rui_castro@live.com.pt>
2024-12-14 19:50:19 -0100

    Trying to fix css styling issue

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ const gitCommitHash = execSync('git rev-parse --short --verify HEAD').toString()
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		postcss: './postcss.config.js'
+	},
 	define: {
 		__APP_VERSION__: JSON.stringify(gitCommitHash)
 	}
