f75244451588ce26cfbba4439b3aff98bcf93e02
Rui Castro <rui_castro@live.com.pt>
2024-12-14 19:09:37 -0100

    Added app version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
 	import Introduction from '../components/Introduction.svelte';
 	import Pictures from '../components/Pictures.svelte';
 	import Reviews from '../components/Reviews.svelte';
-	import { PUBLIC_BUILD_VERSION } from '$env/static/public';
 </script>
 
 <svelte:head>
@@ -11,7 +10,7 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div data-version="${PUBLIC_BUILD_VERSION ?? null}">
+<div data-version={__APP_VERSION__}>
 	<HeroImage />
 	<div class="content">
 		<Introduction />
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { execSync } from 'child_process';
+
+const gitCommitHash = execSync('git rev-parse --short --verify HEAD').toString().trim();
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		__APP_VERSION__: JSON.stringify(gitCommitHash)
+	}
 });
